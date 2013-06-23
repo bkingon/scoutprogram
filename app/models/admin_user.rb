ecb171be3c96ebf6b449bc74aa920401f7fb79b8
Brad Kingon <bkingon@gmail.com>
2013-06-23 16:25:02 +0200

    removed clerk stuff

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,6 @@
 class AdminUser < ActiveRecord::Base
-  include SentientUser
-  track_who_does_it
+  # include SentientUser
+  # track_who_does_it
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
